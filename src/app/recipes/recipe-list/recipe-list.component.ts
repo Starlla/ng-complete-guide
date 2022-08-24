6e6c8c8ee015135a98061c207a0b7c2a36efe83d
Claire <claire.cy.t@gmail.com>
2022-08-24 10:23:09 -0400

    put recipes data into service

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -8,15 +9,14 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
-  recipes: Recipe[] = [new Recipe('A Test Recipe', "Just a test", "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F44%2F2020%2F03%2F03%2F7782449.jpg"),
-  new Recipe('Another Test Recipe', "Just another test", "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F44%2F2020%2F03%2F03%2F7782449.jpg"),]
+  recipes: Recipe[];
 
-
-  constructor() {
+  constructor(private recipeService: RecipeService) {
 
   }
 
   ngOnInit(): void {
+    this.recipes = this.recipeService.getRecipes()
   }
 
   onRecipeSelected(recipe: Recipe) {
---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,12 @@
+import { Recipe } from "./recipe.model";
+
+export class RecipeService {
+  private recipes: Recipe[] = [new Recipe('A Test Recipe', "Just a test", "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F44%2F2020%2F03%2F03%2F7782449.jpg"),
+  new Recipe('Another Test Recipe', "Just another test", "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F44%2F2020%2F03%2F03%2F7782449.jpg"),]
+
+
+  getRecipes() {
+    return this.recipes.slice();
+  }
+
+}
