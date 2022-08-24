f9db421c759f7fb1ef9ab6abdfa202900bd50bec
Claire <claire.cy.t@gmail.com>
2022-08-24 01:17:33 -0400

    add diretive

---
+++ src/app/shared/dropdown-directive.ts
@@ -0,0 +1,24 @@
+import { Directive, ElementRef, HostBinding, HostListener, Renderer2 } from "@angular/core";
+
+@Directive({
+  selector: '[appDropdown]'
+})
+export class DropdownDirective {
+  private isOpen = false;
+
+  @HostListener('click') onClick() {
+    const dropdown = this.elementRef.nativeElement.querySelector('.dropdown-menu');
+    if (!this.isOpen) {
+      this.renderer.addClass(dropdown, 'show');
+    } else {
+      this.renderer.removeClass(dropdown, 'show');
+    }
+
+    this.isOpen = !this.isOpen;
+  }
+
+  constructor(private elementRef: ElementRef,
+    private renderer: Renderer2) {
+  }
+}
+
