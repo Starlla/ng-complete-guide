e9932f3fa3c7a407398579cc2b2c5b460a7b48d2
Claire <claire.cy.t@gmail.com>
2022-08-24 11:36:38 -0400

    fix dropdown style

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,11 +1,17 @@
 import { EventEmitter } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
 import { Recipe } from "./recipe.model";
 
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
-  private recipes: Recipe[] = [new Recipe('A Test Recipe', "Just a test", "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F44%2F2020%2F03%2F03%2F7782449.jpg"),
-  new Recipe('Another Test Recipe', "Just another test", "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F44%2F2020%2F03%2F03%2F7782449.jpg"),]
+  private recipes: Recipe[] = [new Recipe('A Test Recipe',
+    "Just a test", "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F44%2F2020%2F03%2F03%2F7782449.jpg",
+    [new Ingredient('peach', 5), new Ingredient('Meat', 6)]
+  ),
+
+  new Recipe('Another Test Recipe', "Just another test", "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F44%2F2020%2F03%2F03%2F7782449.jpg",
+    [new Ingredient('peach', 5), new Ingredient('Meat', 6)]),]
 
 
   getRecipes() {
